65688240e1fd943e422a5ed34a770d35f879fb3a
aryanbhardwaj425 <bhardwajaryan425@gmail.com>
2025-01-21 09:12:21 +0000

    test script added

---
+++ jenkins/test.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+
+echo "project is being tested"
+
+
+
+echo "Node.js version:"
+node -v
+
+
+echo "npm version:"
+npm -v
+
+echo "tests are running : "
+npm run test
+
+
+echo "Tests completed"
